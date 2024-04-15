d772e7fb540b80397fa81d92ef7249c9027e40fd
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-04-15 21:58:26 +0500

    Add styles

--- build.mjs
+++ build.mjs
@@ -3,6 +3,7 @@ import * as fs from 'fs/promises';
 
 await fs.copyFile('./src/index.html', './dist/index.html');
 await fs.copyFile('./src/app/app.component.html', './dist/app.component.html');
+await fs.copyFile('./src/app/app.component.css', './dist/app.component.css');
 
 await esbuild.build({
   entryPoints: ['./src/main.ts'],
