3602da7aabb2c91a33742e223a0e66a06fec8710
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-04-16 21:07:34 +0500

    Externalize @angular/core

--- build.mjs
+++ build.mjs
@@ -5,11 +5,17 @@ await fs.mkdir('./dist', {recursive: true});
 await fs.copyFile('./src/index.html', './dist/index.html');
 
 await esbuild.build({
-  entryPoints: ['./src/main.ts'],
+  entryPoints: [
+    './src/main.ts',
+    './node_modules/@angular/core/fesm2022/core.mjs',
+    './node_modules/@angular/core/fesm2022/primitives/signals.mjs',
+  ],
   bundle: true,
-  outfile: './dist/app.mjs',
+  outdir: './dist',
+  entryNames: '[dir]/[name]',
   format: 'esm',
   sourcemap: true,
   tsconfig: './tsconfig.json',
   loader: { '.css': 'text', '.html': 'text' },
+  external: ['@angular/core']
 })
