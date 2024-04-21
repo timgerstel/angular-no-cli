cf3b62f5941dfc0e9b2625c2d5dd881d42ae9270
Leonty Chudinov <leonty.chudinov@gmail.com>
2024-04-21 10:05:00 +0500

    Add styles to plugin

--- src/plugin/plugin.component.ts
+++ src/plugin/plugin.component.ts
@@ -3,8 +3,8 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'app-plugin',
-    template: '<div> {{ title }} </div>',
-    styles: [],
+    templateUrl: './plugin.component.html',
+    styleUrl: './plugin.component.css',
     standalone: true
 })
 export class PluginComponent {
